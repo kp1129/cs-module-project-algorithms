f56cc43902e8a99525dd27e7a75b459fc868a785
kp1129 <katya.p.1129@gmail.com>
2020-07-23 12:44:56 -0700

    refactor single_number, moving_zeroes, and eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,19 +2,49 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
-    # counter = []
+# def eating_cookies(n):
+#     # Your code here
+#     # counter = []
+#     if n < 0 :
+#         return 0
+#     elif n == 0 or n == 1:
+#         return 1
+#     elif n == 2:
+#         return 2
+#     elif n == 3: 
+#         return 4
+#     else:
+#         return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+
+# a better solution
+def eating_cookies(n, cache={}):
+    # base case: if less than 0 then it's not a valid path
+    # if 0, then this is a valid path to 0 cookies
+    # (not that there is 1 way to eat 0 cookies, fyi)
+
+    # in addition to lowering the number of base cases,
+    # a huge improvement would be to add a cache
+    # this way, the algorithm doesn't have to repeat 
+    # computations
+
+    # initiate cache as a second param in the function as an empty dict
+    # think of cache as state in react.js
+
+    # check the cache before running any new computations
     if n < 0 :
         return 0
-    elif n == 0 or n == 1:
+    elif n == 0:
         return 1
-    elif n == 2:
-        return 2
-    elif n == 3: 
-        return 4
+    elif n in cache:
+        # don't go on to recursion, just return previously made computation
+        return cache[n]
     else:
-        return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+        # run eating_cookies recursively and store the result in cache[n]
+        cache[n] = eating_cookies(n-3, cache) + eating_cookies(n-2, cache) + eating_cookies(n-1, cache) 
+    # return what's stored in cache for that n value
+    return cache[n]    
+
+
 
 
 if __name__ == "__main__":
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,18 +2,38 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+# def single_number(arr):
+#     # Your code here
+#     temp = []
+#     doubles = []
+#     for i in arr:
+#         if i not in temp:
+#             temp.append(i)
+#         elif i in temp:
+#             temp.pop(temp.index(i))
+#             doubles.append(i)
+    
+#     return temp[0]
+
+
+# a better solution, now its O(n) 
+# because it's 2 sequential for loops 
+# and we're not searching an array for 
+# an item (which isnt great) but a dict
+
 def single_number(arr):
     # Your code here
-    temp = []
-    doubles = []
+    counts = {}
     for i in arr:
-        if i not in temp:
-            temp.append(i)
-        elif i in temp:
-            temp.pop(temp.index(i))
-            doubles.append(i)
+        if i not in counts:
+            counts[i] = 1
+        else:
+            counts[i] += 1
     
-    return temp[0]
+    for k, v in counts.items():
+        if v == 1:
+            return k
+
 
 
 if __name__ == '__main__':
